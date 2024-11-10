d58edbfcbfbd19adaffd4c338dc2b257d6f768a9
Taha Gaber <tahagaber175@gmail.com>
2024-11-10 20:13:47 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+let stars1 = document.getElementById('stars1');
+let moon2 = document.getElementById('moon2');
+let mountains3 = document.getElementById('mountains3');
+let mountains4 = document.getElementById('mountains4');
+let river5 = document.getElementById('river5');
+let boat6 = document.getElementById('boat6');
+let mountains7 = document.getElementById('mountains7');
+let nouvil  = document.querySelector('.nouvil');
+
+window.onscroll = function(){
+    let value = scrollY;
+    stars1.style.left  = value + 'px';
+    moon2.style.top = value * 3 + 'px';
+    mountains3.style.top = value * 2 + 'px';
+    mountains4.style.top = value * 1.5 + 'px';
+    river5.style.top = value * 0.8 + 'px';
+    boat6.style.top = value * 0.8 + 'px';
+    boat6.style.left = value * 1 + 'px';
+    nouvil.style.fontSize = value + 'px';
+    if(scrollY >= 67){
+        nouvil.style.fontSize = 67 + 'px' ;
+        nouvil.style.position =  'fixed';
+        if(scrollY >=441.6000061035156){
+            nouvil.style.display = 'none';
+
+        }
+        else{
+            nouvil.style.display = 'block'
+        }
+    }
+}
